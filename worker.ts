b0de731b77514f99d8a4e846dabafad838cabdd0
max-lt <maxime.tissier@gmail.com>
2021-09-25 20:50:45 +0200

    1 file modified

--- worker.ts
+++ worker.ts
@@ -1,8 +1,13 @@
-addEventListener('fetch', (event: FetchEvent) => {
-  event.respondWith(
-    new Response('Hello deno!', {
-      status: 200,
-      headers: { 'content-type': 'text/plain' },
-    })
-  );
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+addEventListener('fetch', async (event: FetchEvent) => {
+  const response = app.handle(event.request);
+
+  event.respondWith(response);
 });
