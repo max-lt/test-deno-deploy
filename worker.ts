3c84b7fe46708a0d4a4773c775db9d69009eb6b6
max-lt <maxime.tissier@gmail.com>
2021-09-25 20:55:48 +0200

    2 files modified

--- worker.ts
+++ worker.ts
@@ -1,10 +1,30 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
+
+const books = new Map<string, any>();
+books.set('1', {
+  id: '1',
+  title: 'The Hound of the Baskervilles',
+  author: 'Conan Doyle, Arthur',
+});
 
 const app = new Application();
+const router = new Router();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!!";
+router.get('/', (context) => {
+  context.response.body = 'Hello world!';
 });
+
+router.get('/book', (context) => {
+  context.response.body = Array.from(books.values());
+});
+
+router.get('/book/:id', (context) => {
+  if (books.has(context?.params?.id)) {
+    context.response.body = books.get(context.params.id);
+  }
+});
+
+app.use(router.routes());
 
 addEventListener('fetch', async (event: FetchEvent) => {
   const response = app.handle(event.request);
