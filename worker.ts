2614d90887ca96195794599606368178fe45cddf
max-lt <maxime.tissier@gmail.com>
2021-09-25 20:51:07 +0200

    1 file modified

--- worker.ts
+++ worker.ts
@@ -3,7 +3,7 @@ import { Application } from 'https://deno.land/x/oak/mod.ts';
 const app = new Application();
 
 app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+  ctx.response.body = "Hello World!!";
 });
 
 addEventListener('fetch', async (event: FetchEvent) => {
