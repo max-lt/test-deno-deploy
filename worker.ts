db21f8844d21ade78be6d0b6b479dc4d5c21fba1
max-lt <maxime.tissier@gmail.com>
2021-09-25 20:37:03 +0200

    1 file modified

--- worker.ts
+++ worker.ts
@@ -1,10 +1,3 @@
-import { router } from 'https://crux.land/router@0.0.4';
-
-const router = router({
-  'GET@/': (_req) => new Response('Hello get!', { status: 200 }),
-  'POST@/': (_req) => new Response('Hello post!', { status: 200 }),
-});
-
 addEventListener('fetch', (event: FetchEvent) => {
   event.respondWith(
     new Response('Hello deno!', {
