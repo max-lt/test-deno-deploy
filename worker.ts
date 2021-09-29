39630636f32035e7b9a276efa3f829728074b6e0
max-lt <maxime.tissier@gmail.com>
2021-09-29 18:15:24 +0200

    1 file modified

--- worker.ts
+++ worker.ts
@@ -12,9 +12,8 @@ async function handleRequest(request) {
   const { pathname } = new URL(request.url);
 
   return fetch('https://devcat.fr/' + pathname).then((response) => {
-    return new Response(response.body, {
-      ...response,
-      headers: { 'x-debug-pathname': pathname, ...response.headers },
-    });
+    const headers = new Headers(response.headers);
+    headers.set('x-debug-pathname', pathname);
+    return new Response(response.body, { ...response, headers });
   });
 }
