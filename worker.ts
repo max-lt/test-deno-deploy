6efc3ea69eba8abb8661363fbb46876280010419
max-lt <maxime.tissier@gmail.com>
2021-09-29 17:59:04 +0200

    1 file modified

--- worker.ts
+++ worker.ts
@@ -1,33 +1,16 @@
-import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
+// deno-lint-ignore-file require-await
+addEventListener('fetch', (event) => {
+  console.log('Worker: fetch event');
 
-const books = new Map<string, any>();
-books.set('1', {
-  id: '1',
-  title: 'The Hound of the Baskervilles',
-  author: 'Conan Doyle, Arthur',
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
 });
 
-const app = new Application();
-const router = new Router();
+async function handleRequest(request) {
+  const { pathname } = new URL(request.url);
 
-router.get('/', (context) => {
-  context.response.body = 'Hello world!!';
-});
-
-router.get('/books', (context) => {
-  context.response.body = Array.from(books.values());
-});
-
-router.get('/book/:id', (context) => {
-  if (books.has(context?.params?.id)) {
-    context.response.body = books.get(context.params.id);
-  }
-});
-
-app.use(router.routes());
-
-addEventListener('fetch', async (event: FetchEvent) => {
-  const response = app.handle(event.request);
-
-  event.respondWith(response);
-});
+  return fetch('https://devcat.fr/' + pathname);
+}
